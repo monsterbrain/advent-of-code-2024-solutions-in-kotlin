4a9659670a7341f4927ceaf538ed9a8eb43f1025
Faisal rasak <monsterkillu@gmail.com>
2024-12-01 12:09:47 +0530

    Day-01 solutions uploaded ..

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,57 @@
+import kotlin.math.abs
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val inRegex = """(\d+)\s*(\d+)""".toRegex()
+        val lefts = mutableListOf<Int>()
+        val rights = mutableListOf<Int>()
+        input.forEach {
+            val (left, right) = inRegex.matchEntire(it)?.destructured ?: throw IllegalArgumentException("Bad input")
+            println("left,right = [$left, $right]")
+            lefts.add(left.toInt())
+            rights.add(right.toInt())
+        }
+        lefts.sort()
+        rights.sort()
+
+        var sum = 0
+        // note: swapped the params it, i instead of i, it, and hence the sum wasn't getting right ...
+        lefts.forEachIndexed {i ,it ->
+            sum += abs(it-rights[i])
+        }
+
+        return sum
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        val inRegex = """(\d+)\s*(\d+)""".toRegex()
+        val lefts = mutableListOf<Int>()
+        val rights = mutableListOf<Int>()
+        input.forEach {
+            val (left, right) = inRegex.matchEntire(it)?.destructured ?: throw IllegalArgumentException("Bad input")
+            lefts.add(left.toInt())
+            rights.add(right.toInt())
+        }
 
-    // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+        var sum = 0
+        lefts.forEach { left ->
+            val countInRightList = rights.count { it == left }
+            sum += countInRightList * left
+        }
+
+
+        return sum
+    }
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 11)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
-    part1(input).println()
+//    part1(input).println()
+
+    check(part2(testInput) == 31)
+
     part2(input).println()
 }
